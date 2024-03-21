1838bc79416309fe3e3f6416e022d03381f63840
CORP\shivakumar.k <shivakumar.k@glowtouch.om>
2024-03-21 17:25:09 +0530

    added PORT 3000

--- index.js
+++ index.js
@@ -107,5 +107,5 @@ app.delete('/employees/:id', getEmployee, async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
